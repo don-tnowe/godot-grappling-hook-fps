f8b019d005c8f1a67c5ba2fd46c08a1ec3d77316
don-tnowe <67479453+don-tnowe@users.noreply.github.com>
2022-08-20 03:03:54 +0300

    Improve touch controls UX

--- src/Hero/TouchscreenHUD.gd
+++ src/Hero/TouchscreenHUD.gd
@@ -4,12 +4,13 @@ extends CanvasLayer
 signal tilt_move(direction)
 signal drag_look(offset)
 
-export var tilt_deadzone := PI * 0.25
-export var tilt_sensitivity := 48.0 / PI
+export var tilt_deadzone := PI * 0.15
+export var tilt_sensitivity := 32.0 / PI
 export var look_minimum_x := 640
 
 var is_touch := true
 var anchor_basis := Basis()
+var gyro := Vector3.ZERO
 
 
 func _ready():
@@ -21,14 +22,22 @@ func _ready():
 	anchor_current_tilt(-Input.get_gravity().normalized())
 
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	if !is_touch: return
 	var gravity_up = -Input.get_gravity().normalized()
 	
 	if Input.is_action_pressed("move_stop"):
 		anchor_current_tilt(gravity_up)
+		gyro = Vector3.ZERO
 
-	var gravity_vec = anchor_basis.xform(gravity_up)
+	gyro += Input.get_gyroscope() * delta
+
+	# Every commented method here tilts movement vector to the left on test device. TODO: find a way to consistently correct this
+	#	var gravity_vec = anchor_basis.xform(gravity_up)
+	# var gravity_vec = anchor_basis.xform(_drift_correction(Basis(gyro), gravity_up).y)
+	# var gravity_vec = _drift_correction(Basis(gyro), gravity_up).y
+	var gravity_vec = Vector2(-gyro.y, gyro.x)
+
 	var input_vec = Vector2(-gravity_vec.x * tilt_sensitivity, gravity_vec.y * tilt_sensitivity).limit_length(1)
 	if input_vec.length_squared() > tilt_deadzone * tilt_deadzone:
 		emit_signal("tilt_move", input_vec)
@@ -46,6 +55,20 @@ func anchor_current_tilt(gravity_up):
 		gravity_up.cross(Vector3.UP).normalized(),
 		gravity_up.angle_to(Vector3.UP) - PI * 0.5
 	)
+
+
+func _drift_correction(p_basis, p_grav):
+	# start by calculating the dot product, this gives us the cosine angle between our two vectors
+	var dot = p_basis.y.dot(p_grav)
+
+	# if our dot is 1.0 we're good
+	if dot < 1.0:
+		# the cross between our two vectors gives us a vector perpendicular to our two vectors
+		var axis = p_basis.y.cross(p_grav).normalized()
+		var correction = Basis(axis, acos(dot))
+		p_basis = correction * p_basis
+
+	return p_basis
 
 
 func _input(event):
